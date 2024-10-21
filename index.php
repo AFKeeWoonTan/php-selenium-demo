0430c1b6a39ebc43560b11c201847758ecd04a19
Marcus Tan <marcus@cloakapps.com>
2024-10-21 19:34:42 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Welcome to PHP Selenium Testing</h1>";
+echo "<a href='https://www.php.net' class='php-link'>Learn PHP</a>";
+?>
+
---
+++ tests/SeleniumTest.php
@@ -0,0 +1,34 @@
+<?php
+require 'vendor/autoload.php';
+
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\WebDriverBy;
+
+class SeleniumTest {
+    public function testExample() {
+        $serverUrl = 'http://localhost:4444/wd/hub'; // Selenium server URL
+        $driver = RemoteWebDriver::create($serverUrl, DesiredCapabilities::chrome());
+
+        // Navigate to the PHP application
+        $driver->get('http://localhost:8000/index.php');
+
+        // Wait and get the text of the PHP link
+        $element = $driver->findElement(WebDriverBy::className('php-link'));
+        $text = $element->getText();
+
+        // Assert the text
+        if ($text == 'Learn PHP') {
+            echo "Test Passed\n";
+        } else {
+            echo "Test Failed\n";
+        }
+
+        // Close the browser
+        $driver->quit();
+    }
+}
+
+$test = new SeleniumTest();
+$test->testExample();
+
