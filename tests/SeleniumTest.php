aa8c7e8d730b8e9782608f44ecfeb9ab9f1047af
Marcus Tan <marcus@cloakapps.com>
2024-10-22 11:15:57 +0800

    selenium tests

--- tests/SeleniumTest.php
+++ tests/SeleniumTest.php
@@ -7,13 +7,12 @@ use Facebook\WebDriver\WebDriverBy;
 
 class SeleniumTest {
     public function testExample() {
-        $serverUrl = 'http://localhost:4444/wd/hub'; // Selenium server URL
+        $serverUrl = 'http://selenium:4444/wd/hub'; // Selenium server URL
         $driver = RemoteWebDriver::create($serverUrl, DesiredCapabilities::chrome());
 
         // Navigate to the PHP application
-        $driver->get('http://localhost:8000/index.php');
-	//$driver->get('http://host.docker.internal:8000/index.php');
-        //$driver->get('http://127.0.0.1:8000/index.php');
+        $driver->get('http://testserver/index.php');
+        //$driver->get('http://localhost:8000/index.php');
 
         // Wait and get the text of the PHP link
         $element = $driver->findElement(WebDriverBy::className('php-link'));
